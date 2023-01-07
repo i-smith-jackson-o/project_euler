709bfd96e5deb1b7a0cd47f84f29be35151cb9ad
i-smith-jackson-o <i.smith.jackson.o@gmail.com>
2023-01-06 23:41:12 -0500

    experimenting with tex, added directories for implementing solutions in other languages when I get around to learning them

--- js/problem_1.js
+++ js/problem_1.js
@@ -6,13 +6,13 @@ find the sum of all the multiples of 3 or 5 below 1000
 */
 
 function threeOrFive(num) {
-	let sum = 0; 
-	for (let i = 0; i < num; i++) {
-		if (i % 3 == 0 || i % 5 == 0) {
-			sum += i; 
-		}
-	}
-	return sum; 
+    let sum = 0; 
+    for (let i = 0; i < num; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i; 
+        }
+    }
+    return sum; 
 }
 
 console.log(threeOrFive(1000)); 
