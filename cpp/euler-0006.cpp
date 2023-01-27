29376679e861eb909b20ebdd38440af227bc3a8f
i-smith-jackson-o <i.smith.jackson.o@gmail.com>
2023-01-27 11:07:45 -0500

    started 0006

--- cpp/euler-0006.cpp
+++ cpp/euler-0006.cpp
@@ -1,2 +1,15 @@
-//sum square difference. 
+/*
+ * The sum of the squares of the first ten natural numbers is, 
+ * 1^2 + 2^2 + ... + 10^2 == 385
+ *
+ * the square of the sun of the first ten natural numbers is, 
+ * (1 + 2 + ... + 10)^2 = 55^2 = 3025
+ *
+ * Hence the difference between the sun of the squares of the first ten natural 
+ * numbers and the square of their sum is 3025 - 385 = 2640 
+ *
+ * Find the difference between the sun of the squares of the first one hundred 
+ * natural numbers and the square of the sum 
+ */
+
 
