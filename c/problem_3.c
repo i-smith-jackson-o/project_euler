b194e04436129e3cc60dd2ab1bed22f29a63b617
i-smith-jackson-o <i.smith.jackson.o@gmail.com>
2023-02-13 20:37:11 -0500

    completed problem3

---
+++ c/problem_3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+
+long maxFactor(long n) {
+    double maxFactor = sqrt(n); 
+    int factor, lastFactor; 
+
+    if (n % 2 == 0) {
+	lastFactor = 2; 
+	n = n / 2; 
+    } else {
+	lastFactor = 1; 
+    }
+    factor = 3; 
+
+    while(n > 1 && factor <= maxFactor) {
+	if (n % factor == 0) {
+	    n = n / factor; 
+	    lastFactor = factor; 
+	    while(n % factor == 0) {
+		n = n / factor; 
+		maxFactor = sqrt(n); 
+	    }
+	}
+	factor = factor + 2; 
+
+	if (n == 1) {
+	    return lastFactor; 
+	} else {
+	    continue; 
+	}
+    }
+    return n; 
+}
+
+int main(void) {
+    long bigNum= 600851475143; 
+    printf("%lu\n", maxFactor(bigNum)); 
+    return 0; 
+}
