2c4333e04d786fcbfe2a78d7ba8af9261998f4a0
i-smith-jackson-o <i.smith.jackson.o@gmail.com>
2023-02-13 20:41:02 -0500

    clang format

--- c/problem_1.c
+++ c/problem_1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-    int sum = 0; 
+  int sum = 0;
 
-    for (int i = 0; i < 1000; i++) {
-	if (i % 3 == 0 || i % 5 == 0) {
-	    sum += i; 
-	}
+  for (int i = 0; i < 1000; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
     }
-    printf("%d", sum); 
+  }
+  printf("%d", sum);
 }
--- c/problem_3.c
+++ c/problem_3.c
@@ -1,40 +1,40 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
 long maxFactor(long n) {
-    double maxFactor = sqrt(n); 
-    int factor, lastFactor; 
+  double maxFactor = sqrt(n);
+  int factor, lastFactor;
 
-    if (n % 2 == 0) {
-	lastFactor = 2; 
-	n = n / 2; 
+  if (n % 2 == 0) {
+    lastFactor = 2;
+    n = n / 2;
+  } else {
+    lastFactor = 1;
+  }
+  factor = 3;
+
+  while (n > 1 && factor <= maxFactor) {
+    if (n % factor == 0) {
+      n = n / factor;
+      lastFactor = factor;
+      while (n % factor == 0) {
+        n = n / factor;
+        maxFactor = sqrt(n);
+      }
+    }
+    factor = factor + 2;
+
+    if (n == 1) {
+      return lastFactor;
     } else {
-	lastFactor = 1; 
+      continue;
     }
-    factor = 3; 
-
-    while(n > 1 && factor <= maxFactor) {
-	if (n % factor == 0) {
-	    n = n / factor; 
-	    lastFactor = factor; 
-	    while(n % factor == 0) {
-		n = n / factor; 
-		maxFactor = sqrt(n); 
-	    }
-	}
-	factor = factor + 2; 
-
-	if (n == 1) {
-	    return lastFactor; 
-	} else {
-	    continue; 
-	}
-    }
-    return n; 
+  }
+  return n;
 }
 
 int main(void) {
-    long bigNum= 600851475143; 
-    printf("%lu\n", maxFactor(bigNum)); 
-    return 0; 
+  long bigNum = 600851475143;
+  printf("%lu\n", maxFactor(bigNum));
+  return 0;
 }
