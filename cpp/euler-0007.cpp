cd0a3f4ea16e4df587f871ef0db822eeb1bff74b
i-smith-jackson-o <i.smith.jackson.o@gmail.com>
2023-02-18 01:48:58 -0500

    Struggling with this one a little bit.

--- cpp/euler-0007.cpp
+++ cpp/euler-0007.cpp
@@ -2,7 +2,21 @@
 #include <iostream>
 #include <vector>
 
+bool isPrime(int n) {
+  bool isPrime = true; 
+  if (n == 0 || n == 1) isPrime = false; 
+  for (int i = 2; i <= n/2; ++i) {
+    if (n % i == 0) {
+      isPrime = false; 
+      break; 
+    }
+  }
+  return isPrime; 
+}
+
 int main() {
+  std::vector<int> primes {}; 
+
 
   return 0; 
 }
