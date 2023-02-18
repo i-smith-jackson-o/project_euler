e9cd82c1e3e174f42aaeb0ae65c363f4613f85be
i-smith-jackson-o <i.smith.jackson.o@gmail.com>
2023-02-17 23:26:58 -0500

    completed 0006

--- cpp/euler-0006.cpp
+++ cpp/euler-0006.cpp
@@ -1,15 +1,20 @@
-/*
- * The sum of the squares of the first ten natural numbers is, 
- * 1^2 + 2^2 + ... + 10^2 == 385
- *
- * the square of the sun of the first ten natural numbers is, 
- * (1 + 2 + ... + 10)^2 = 55^2 = 3025
- *
- * Hence the difference between the sun of the squares of the first ten natural 
- * numbers and the square of their sum is 3025 - 385 = 2640 
- *
- * Find the difference between the sun of the squares of the first one hundred 
- * natural numbers and the square of the sum 
- */
+#include <iostream>
+
+int main() {
+  int n = 100; 
+  int sum {}; 
+  int sumSquares {}; 
+
+  for (int i = 0; i <= n; i++) {
+    sumSquares += i * i; 
+  }
+
+  for (int i = 0; i <= n; ++i) {
+    sum += i; 
+  }
+
+  std::cout << (sum * sum) - sumSquares << std::endl; 
+  return 0; 
+}
 
 
